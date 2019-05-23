7a6259da442b991f6984e13408638aad04cd2a12
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-05-23 13:38:50 +0100

    :sparkles: Transaction class stores Date, Amount, TransactionType and CurrentBalance. Tests passing.

---
+++ Properties/Main/Transaction.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace Bank_In_Csharp.Properties.Main
+{
+    public class Transaction
+    {
+        public string transactionDate;
+        public double transactionAmount;
+        public string transactionType;
+        public double currentBalance;
+
+        public Transaction(string date, double amount, string type, double balance)
+        {
+            SetTransactionDate(date);
+            SetTransactionAmount(amount);
+            SetTransactionType(type);
+            SetCurrentBalance(balance);
+        }
+
+        public void SetTransactionDate(string date)
+        {
+            transactionDate = date;
+        }
+
+        public string GetTransactionDate()
+        {
+            return transactionDate;
+        }
+
+        public void SetTransactionAmount(double amount)
+        {
+            transactionAmount = amount;
+        }
+
+        public double GetTransactionAmount()
+        {
+            return transactionAmount;
+        }
+
+        public void SetTransactionType(string type)
+        {
+            transactionType = type;
+        }
+
+        public string GetTransactionType()
+        {
+            return transactionType;
+        }
+
+        public void SetCurrentBalance(double balance)
+        {
+            currentBalance = balance;
+        }
+
+        public double GetCurrentBalance()
+        {
+            return currentBalance;
+        }
+    }
+}
---
+++ Properties/Test/Unit_Tests/TranscationTest.cs
@@ -0,0 +1,36 @@
+﻿using Bank_In_Csharp.Properties.Main;
+using NUnit.Framework;
+using System;
+namespace Bank_In_Csharp.Properties.Test.Unit_Tests
+{
+    public class TranscationTest
+    {
+        [Test, Description("Creating a new instance of transaction sets transactionDate")]
+        public void SetTransactionDate()
+        {
+            Transaction transaction = new Transaction("23/05/19", 10.00, "Deposit", 20.00);
+            Assert.AreEqual("23/05/19", transaction.GetTransactionDate());
+        }
+
+        [Test, Description("Creating a new instance of transaction sets transactionAmount")]
+        public void SetTransactionAmount()
+        {
+            Transaction transaction = new Transaction("23/05/19", 10.00, "Deposit", 20.00);
+            Assert.AreEqual(10.00, transaction.GetTransactionAmount());
+        }
+
+        [Test, Description("Creating a new instance of transaction sets transactionType")]
+        public void SetTransactionType()
+        {
+            Transaction transaction = new Transaction("23/05/19", 10.00, "Deposit", 20.00);
+            Assert.AreEqual("Deposit", transaction.GetTransactionType());
+        }
+
+        [Test, Description("Creating a new instance of transaction sets transactionBalance")]
+        public void SetCurrentBalance()
+        {
+            Transaction transaction = new Transaction("23/05/19", 10.00, "Deposit", 20.00);
+            Assert.AreEqual(20.00, transaction.GetCurrentBalance());
+        }
+    }
+}
