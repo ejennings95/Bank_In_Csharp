7aa496a049c7aeb66f8921ac678f21fd41ed6eef
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-05-23 15:45:36 +0100

    :sparkles: Account transactions can be displayed using Statement class.

---
+++ Properties/Main/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Bank_In_Csharp.Properties.Main;
+
+namespace Bank_In_Csharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Account account = new Account();
+            account.Deposit(1500.00);
+            account.Deposit(1000.00);
+            account.Deposit(100.00);
+            account.Withdrawl(1500.00);
+
+            Statement statement = new Statement(account.GetTransactions());
+
+            statement.DisplayStatement();
+
+        }
+    }
+}
---
+++ Properties/Main/Statement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank_In_Csharp.Properties.Main
+{
+    public class Statement
+    {
+        public List<Transaction> transactions = new List<Transaction>();
+        public StringBuilder statement = new StringBuilder();
+
+        public Statement(List<Transaction> accountTransactions)
+        {
+            SetTransactions(accountTransactions);
+        }
+
+        public void SetTransactions(List<Transaction> accountTransactions)
+        {
+            transactions = accountTransactions;
+        }
+
+        public List<Transaction> GetTransactions()
+        {
+            return transactions;
+        }
+
+        public StringBuilder DisplayStatement()
+        {
+            statement.Append("date || credit || debit || balance\n");
+            foreach (var transaction in transactions)
+            {
+                statement.Append(transaction.GetTransactionDate());
+                if (transaction.GetTransactionType() == "Deposit")
+                {
+                    statement.Append(" || " + transaction.GetTransactionAmount() + " ||");
+                } else if (transaction.GetTransactionType() == "Withdrawl")
+                {
+                    statement.Append(" || || " + transaction.GetTransactionAmount());
+                }
+                statement.Append(" || " + transaction.GetCurrentBalance() + "\n");
+            }
+            Console.WriteLine(statement);
+            return statement;
+        }
+    }
+}
