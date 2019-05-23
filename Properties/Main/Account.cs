fc0a75163db8bc828db6bcf87359078879f7320e
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-05-23 13:11:35 +0100

    :sparkles: Can deposit money into account balance. Test passing.

---
+++ Properties/Main/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Bank_In_Csharp.Properties.Main
+{
+    public class Account
+    {
+        public double balance = 0.00;
+
+        public Account()
+        {
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public void SetBalance(double accountBalance)
+        {
+            balance = accountBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            SetBalance(balance + amount); 
+        } 
+    }
+}
---
+++ Properties/Test/Unit_Tests/AccountTest.cs
@@ -0,0 +1,24 @@
+﻿using Bank_In_Csharp.Properties.Main;
+using NUnit.Framework;
+using System;
+namespace Bank_In_Csharp.Properties.Test.Unit_Tests
+{
+    public class AccountTest
+    {
+
+        [Test, Description("Original Balance is 0.00")]
+        public void defaultBalance()
+        {
+            Account account = new Account();
+            Assert.AreEqual(0.00, account.GetBalance());
+        }
+
+    [Test, Description("Deposited money goes into Balance")]
+        public void Deposit()
+        {
+            Account account = new Account();
+            account.Deposit(100.00);
+            Assert.AreEqual(100.00, account.GetBalance());
+        }
+    }
+}
